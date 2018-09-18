88e72d55e38e87eac83077662b3ef1c695e61f94
Evan Furman <evan.furman@frontgatetickets.com>
2018-09-18 12:09:05 -0500

    adding double quotes

--- hello.php
+++ hello.php
@@ -1,5 +1,5 @@
 <?php
-echo 'hello world\n\n';
+echo "hello world\n\n";
 
 $dbname = 'db_name';
 $dbuser = 'db_user';
@@ -8,9 +8,9 @@ $dbhost = 'db_host';
 
 $connection = pg_connect ("host=$dbhost dbname=$dbname user=$dbuser password=$dbpass");
 if($connection) {
-    echo 'connected to postgres\n';
+    echo "connected to postgres\n";
 } else {
-    echo 'there has been an error connecting to postgres\n';
+    echo "there has been an error connecting to postgres\n";
 }
 
 ?>
